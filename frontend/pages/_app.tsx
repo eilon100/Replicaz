8901d6b613ab953c0e9d2b2c8bdcb91c3a88c3b7
eilon <eilonshamir123@gmail.com>
2023-03-01 20:38:38 +0200

    s

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,20 +5,24 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { AuthContextProvider } from "../context/AuthContext";
 import "../styles/imageSwiper.css";
 import Header from "../components/Header/Header";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "../utills/createEmotionCache";
 
 const queryClient = new QueryClient();
-
+const clientSideEmotionCache = createEmotionCache();
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <Toaster />
-        <div className="h-screen w-screen overflow-y-auto bg-slate-100">
-          <Header />
-          <Component {...pageProps} />
-        </div>
-      </AuthContextProvider>
-    </QueryClientProvider>
+    <CacheProvider value={clientSideEmotionCache}>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <Toaster />
+          <div className="h-screen w-screen overflow-y-auto bg-slate-100">
+            <Header />
+            <Component {...pageProps} />
+          </div>
+        </AuthContextProvider>
+      </QueryClientProvider>
+    </CacheProvider>
   );
 }
 
