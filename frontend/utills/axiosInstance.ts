959606ad6ca9c9665a78882764e5d709b4e97700
eilon <eilonshamir123@gmail.com>
2023-03-01 18:53:02 +0200

    s

--- frontend/utills/axiosInstance.ts
+++ frontend/utills/axiosInstance.ts
@@ -8,6 +8,6 @@ export const axiosInstance = axios.create({
     Accept: "application/json",
     "Content-Type": "application/json",
   },
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://replicaz-backend.vercel.app/",
   withCredentials: true,
 });
